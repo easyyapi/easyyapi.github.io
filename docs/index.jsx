1665ef0157ae00cc97888a42c1828de6ee8aeb1b
tangcent <sweettom@foxmail.com>
2019-11-18 20:29:58 +0800

    update version v1.3.4

--- docs/index.jsx
+++ docs/index.jsx
@@ -5,7 +5,7 @@ banner:
   btns:
     - { name: 'Start', href: './documents/index.html', primary: true }
     - { name: 'Github >', href: 'https://github.com/tangcent/easy-yapi' }
-  caption: '当前版本: v1.3.3'
+  caption: '当前版本: v1.3.4'
 features:
   - { name: '多语言', desc: 'Java/Kotlin' }
   - { name: '多框架', desc: 'Spring MVC/Spring boot/Spring WebFlux' }
