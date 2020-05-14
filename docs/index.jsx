8658192abba31f8113f38a433128ab2482c9c265
tangcent <sweettom@foxmail.com>
2020-05-14 19:43:32 +0800

    update version:v1.9.5.0

--- docs/index.jsx
+++ docs/index.jsx
@@ -5,13 +5,13 @@ banner:
   btns:
     - { name: 'Start', href: './documents/index.html', primary: true }
     - { name: 'Github >', href: 'https://github.com/tangcent/easy-yapi' }
-  caption: '当前版本: v1.9.3.0'
+  caption: '当前版本: v1.9.5.0'
 features:
   - { name: '多语言', desc: 'Java/Kotlin/Scala' }
   - { name: '多框架', desc: 'Spring MVC/Spring boot/Spring WebFlux' }
   - { name: '多渠道', desc: 'Postman/Markdown/Yapi' }
   - { name: '丰富的扩展性', desc: '支持多种配置与扩展,满足不同框架与代码规范' }
-  - { name: '调试利器', desc: '无需导出,直接请求' }
+  - { name: '调试利器', desc: '一个小巧的内置类postman工具' }
 
 footer:
   copyRight:
