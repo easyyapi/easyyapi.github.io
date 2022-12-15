520d712ebbe20765fecd2434ce9e47e64075bd80
tangcent <pentatangcent@gmail.com>
2022-12-15 08:03:06 +0800

    update version: v2.5.0

--- docs/index.jsx
+++ docs/index.jsx
@@ -5,7 +5,7 @@ banner:
   btns:
     - { name: 'Start', href: './documents/index.html', primary: true }
     - { name: 'Github >', href: 'https://github.com/tangcent/easy-yapi' }
-  caption: '当前版本: v2.4.4'
+  caption: '当前版本: v2.5.0'
 features:
   - { name: '多语言', desc: 'Java/Kotlin/Scala' }
   - { name: '多框架', desc: 'Spring/Feign/Jaxrs(Quarkus or Jersey)' }
